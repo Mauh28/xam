d1c5e81ff3eaa8c820cc39cdf0ba258ec64b8fdb
Mauh28 <maurogoku2005@gmail.com>
2026-07-07 23:14:40 -0600

    Actualizacion de la pagina de github

--- src/main/java/org/xam/Xam.java
+++ src/main/java/org/xam/Xam.java
@@ -21,3 +21,13 @@ public class Xam {
         XamNetwork.register();
     }
 }
+
+/*
+ * XAM (xd Absolute Mastery)
+ * Copyright (C) 2024 xd Team, Mauh28
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
